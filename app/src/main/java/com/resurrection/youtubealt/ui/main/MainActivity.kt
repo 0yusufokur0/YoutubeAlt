55acfcf2b1eb371aa7dda52346d8bda3c102d7b8
yusuf.okur <yusuf.okur@veripark.com>
2022-03-19 20:50:24 +0300

    NavController was updated.

--- app/src/main/java/com/resurrection/youtubealt/ui/main/MainActivity.kt
+++ app/src/main/java/com/resurrection/youtubealt/ui/main/MainActivity.kt
@@ -2,9 +2,7 @@ package com.resurrection.youtubealt.ui.main
 
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.resurrection.base.core.activity.BaseActivity
 import com.resurrection.youtubealt.R
 import com.resurrection.youtubealt.databinding.ActivityMainBinding
@@ -16,7 +14,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>(
     MainActivityViewModel::class.java
 ) {
 
-     val navController: NavController by lazy { (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment?)?.navController!! }
+    val navController: NavController by lazy { (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment?)?.navController!! }
 
     override fun init(savedInstanceState: Bundle?) {
 
