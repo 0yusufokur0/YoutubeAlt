f8511457665913fe7be90d62327589d45567a8dd
yusuf.okur <yusuf.okur@veripark.com>
2022-03-19 21:43:27 +0300

    AndroidEntryPoint was added.

--- app/src/main/java/com/resurrection/youtubealt/ui/subscriptions/SubscriptionsFragment.kt
+++ app/src/main/java/com/resurrection/youtubealt/ui/subscriptions/SubscriptionsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.resurrection.base.core.fragment.BaseFragment
 import com.resurrection.youtubealt.R
 import com.resurrection.youtubealt.databinding.FragmentSubscriptionsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SubscriptionsFragment : BaseFragment<FragmentSubscriptionsBinding, SubscriptionsViewModel>(
     R.layout.fragment_subscriptions, SubscriptionsViewModel::class.java
 ) {
